0f8a10263e74855bc79c7d031cb8154f19572f4a
tenthman <tenth.man@mail.com>
2024-08-06 15:49:38 +0300

    2

--- card/script.js
+++ card/script.js
@@ -1,63 +1,79 @@
-let card = document.getElementById('card');
-let cardImageHolder = document.getElementById('image-holder');
-let image = document.getElementById('image');
-
-let cardWidthHorizontal;
-// let cardWidthVertical;
-let cardImageHolderHeight;
-
-gorizontalSize(document.documentElement.clientWidth);
-
-console.log(`w: ${image.clientWidth}, h: ${image.clientHeight}`);
-
-if(image.clientWidth > image.clientHeight) {
-	gorizontalSize(document.documentElement.clientWidth);
-} else {
-	verticalSize(document.documentElement.clientWidth);
-}
-
-window.addEventListener("resize", function () {
-	gorizontalSize(document.documentElement.clientWidth);
-});
-
-function gorizontalSize(browserClientWidth) {
-	if(browserClientWidth / 100 * 30 > 400) {
-		cardWidthHorizontal = Math.round(document.documentElement.clientWidth / 100 * 30);
-		cardImageHolderHeight = Math.round(cardWidthHorizontal / 3 * 2);
-	} else {
-		cardWidthHorizontal = 400;
-		cardImageHolderHeight = Math.round(cardWidthHorizontal / 3 * 2);
-	}
-	card.style.width = `${cardWidthHorizontal}px`;
-	// cardImageHolder.style.height = `${cardImageHolderHeight}px`;
-	// image.style.width = '100%';
-	// image.style.hight = 'auto';
-}
-
-function verticalSize(browserClientWidth) {
-	if(browserClientWidth / 100 * 30 > 400) {
-		cardWidthHorizontal = Math.round(document.documentElement.clientWidth / 100 * 30);
-		cardImageHolderHeight = Math.round(cardWidthHorizontal / 2 * 3);
-	} else {
-		cardWidthHorizontal = 400;
-		cardImageHolderHeight = Math.round(cardWidthHorizontal / 2 * 3);
-	}
-	card.style.width = `${cardWidthHorizontal}px`;
-	// cardImageHolder.style.height = `${cardImageHolderHeight}px`;
-	// image.style.width = '100%';
-	// image.style.hight = '100%';
-}
-
-function prevBtnClicked () {
-	console.log("prev button clicked!");
-}
-
-function nextBtnClicked () {
-	console.log("next button clicked!");
-}
-
+let cardImage = document.getElementById('image-holder');
+let navigationPanel = document.querySelector('.navigation');
 let prevButton = document.querySelector(".prev");
-prevButton.addEventListener("click", prevBtnClicked);
-
 let nextButton = document.querySelector(".next");
-nextButton.addEventListener("click", nextBtnClicked);
+
+navigationPanel.style.height = '0';
+
+prevButton.addEventListener("click", () => {alert('prev button clicked! image changed!')});
+nextButton.addEventListener("click", () => {alert('next button clicked! image changed!')});
+
+cardImage.addEventListener("click", () => {alert('image changed!')});
+
+cardImage.addEventListener('mouseenter', () => {navigationPanel.style.height = '50px'})
+cardImage.addEventListener('mouseleave', () => {navigationPanel.style.height = '0'});
+
+navigationPanel.addEventListener('mouseenter', () => {navigationPanel.style.height = '50px'})
+navigationPanel.addEventListener('mouseleave', () => {navigationPanel.style.height = '0'});
+
+
+
+// let card = document.getElementById('card');
+// let cardImageHolder = document.getElementById('image-holder');
+// let image = document.getElementById('image');
+
+// let cardWidthHorizontal;
+// // let cardWidthVertical;
+// let cardImageHolderHeight;
+
+// gorizontalSize(document.documentElement.clientWidth);
+
+// console.log(`w: ${image.clientWidth}, h: ${image.clientHeight}`);
+
+// if(image.clientWidth > image.clientHeight) {
+// 	gorizontalSize(document.documentElement.clientWidth);
+// } else {
+// 	verticalSize(document.documentElement.clientWidth);
+// }
+
+// window.addEventListener("resize", function () {
+// 	gorizontalSize(document.documentElement.clientWidth);
+// });
+
+// function gorizontalSize(browserClientWidth) {
+// 	if(browserClientWidth / 100 * 30 > 400) {
+// 		cardWidthHorizontal = Math.round(document.documentElement.clientWidth / 100 * 30);
+// 		cardImageHolderHeight = Math.round(cardWidthHorizontal / 3 * 2);
+// 	} else {
+// 		cardWidthHorizontal = 400;
+// 		cardImageHolderHeight = Math.round(cardWidthHorizontal / 3 * 2);
+// 	}
+// 	card.style.width = `${cardWidthHorizontal}px`;
+// 	// cardImageHolder.style.height = `${cardImageHolderHeight}px`;
+// 	// image.style.width = '100%';
+// 	// image.style.hight = 'auto';
+// }
+
+// function verticalSize(browserClientWidth) {
+// 	if(browserClientWidth / 100 * 30 > 400) {
+// 		cardWidthHorizontal = Math.round(document.documentElement.clientWidth / 100 * 30);
+// 		cardImageHolderHeight = Math.round(cardWidthHorizontal / 2 * 3);
+// 	} else {
+// 		cardWidthHorizontal = 400;
+// 		cardImageHolderHeight = Math.round(cardWidthHorizontal / 2 * 3);
+// 	}
+// 	card.style.width = `${cardWidthHorizontal}px`;
+// 	// cardImageHolder.style.height = `${cardImageHolderHeight}px`;
+// 	// image.style.width = '100%';
+// 	// image.style.hight = '100%';
+// }
+
+// function prevBtnClicked () {
+// 	console.log("prev button clicked!");
+// }
+
+// function nextBtnClicked () {
+// 	console.log("next button clicked!");
+// }
+
+
